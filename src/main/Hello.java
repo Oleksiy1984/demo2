85fd9a20d894481e62ebc54916322b9c8f1e9ade
Oleksiy <o_orlovskiy@ukr.net>
2017-01-25 00:07:50 +0200

    Initial commit

---
+++ src/main/Hello.java
@@ -0,0 +1,24 @@
+package main;
+
+/**
+ * Created by Zhenichka on 21.11.2016.
+ */
+public class Hello {
+
+    public static class Person {
+        String name;
+
+        public Person(String personName) {
+            name = personName;
+        }
+
+        public String greet(String yourName) {
+            return String.format("Hi %s, my name is %s", yourName,name);
+        }
+    }
+
+    public static void main(String[] args) {
+Person jk=new Person("personName");
+        System.out.println(jk.greet("Alex"));
+    }
+}
